f6b551dbb9a79ca6e4737df68ce1bf79cd25c9cb
ЭФкудфМЭ <arelav90@mail.ru>
2018-05-20 23:19:51 +0300

    add print max function

--- bars.py
+++ bars.py
@@ -5,8 +5,10 @@ def load_data(path):
     data = json.load(open(path, "r", encoding="utf8"))
     return data
 
+
 def get_biggest_bar(data):
-    pass
+    for i in data['features']:
+        print(i['properties']['Attributes']['SeatsCount'])
 
 
 def get_smallest_bar(data):
@@ -20,3 +22,4 @@ def get_closest_bar(data, longitude, latitude):
 if __name__ == '__main__':
     p = "bars.json"
     print(load_data(p))
+    get_biggest_bar(load_data(p))
