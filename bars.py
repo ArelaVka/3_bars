dead58c99c190f5718e3af038b1a89a7c8007bfd
maltsev <arelav90@mail.ru>
2018-05-21 14:29:27 +0300

    add closest bar function

--- bars.py
+++ bars.py
@@ -1,45 +1,38 @@
 import json
 
 
-def load_data(path):
-    data = json.load(open(path, "r", encoding="utf8"))
-    return data
+def load_data(file_path):
+    all_bars_data = json.load(open(file_path, "r", encoding="utf8"))
+    return all_bars_data
 
 
-def get_biggest_bar(data):
-    bb=0
-    for i in data['features']:
-        if bb <= i['properties']['Attributes']['SeatsCount']:
-            bb = i['properties']['Attributes']['SeatsCount']
-            name_of_biggest_bar = i['properties']['Attributes']['Name']
-    print("max seats bar = ", name_of_biggest_bar, " (num of seats - ", bb, ")")
+def get_biggest_bar(input_data):
+    biggest_bar_seats = max(input_data['features'], key=lambda x: x['properties']['Attributes']['SeatsCount'])
+    print("max seats bar = ", biggest_bar_seats['properties']['Attributes']['Name'], " (num of seats - ",
+          biggest_bar_seats['properties']['Attributes']['SeatsCount'], ")")
 
 
-def get_smallest_bar(data):
-    sb=0
-    for i in data['features']:
-        if sb >= i['properties']['Attributes']['SeatsCount']:
-            sb = i['properties']['Attributes']['SeatsCount']
-            name_of_smallest_bar = i['properties']['Attributes']['Name']
-    print("min seats bar = ", name_of_smallest_bar, " (num of seats - ", sb, ")")
+def get_smallest_bar(input_data):
+    smallest_bar_seats = min(input_data['features'], key=lambda x: x['properties']['Attributes']['SeatsCount'])
+    print("min seats bar = ", smallest_bar_seats['properties']['Attributes']['Name'], " (num of seats - ",
+          smallest_bar_seats['properties']['Attributes']['SeatsCount'], ")")
 
 
-def get_closest_bar(data, longitude, latitude):
+def get_closest_bar(input_data, longitude, latitude):
     min_distance = 1000
-    for i in data['features']:
-        distance = ((float(i['geometry']['coordinates'][0]) - longitude) ** 2 +
-                        (float(i['geometry']['coordinates'][1]) - latitude) ** 2) ** (1/2)
+    for current_record in input_data['features']:
+        distance = ((float(current_record['geometry']['coordinates'][0]) - longitude) ** 2 +
+                    (float(current_record['geometry']['coordinates'][1]) - latitude) ** 2) ** (1 / 2)
         if distance < min_distance:
             min_distance = distance
-            closest_bar_name = i['properties']['Attributes']['Name']
+            closest_bar_name = current_record['properties']['Attributes']['Name']
     print("name of closest bar - ", closest_bar_name)
 
 
 if __name__ == '__main__':
-    p = "bars.json"
-    #print(load_data(p))
-    get_biggest_bar(load_data(p))
-    get_smallest_bar(load_data(p))
-    longitude = float (input("Insert longitude: "))
-    latitude = float(input("Insert latitude: "))
-    get_closest_bar(load_data(p), longitude, latitude)
+    path_to_file = "bars.json"
+    get_biggest_bar(load_data(path_to_file))
+    get_smallest_bar(load_data(path_to_file))
+    current_x_coord = float(input("Insert longitude: "))
+    current_y_coord = float(input("Insert latitude: "))
+    get_closest_bar(load_data(path_to_file), current_x_coord, current_y_coord)
