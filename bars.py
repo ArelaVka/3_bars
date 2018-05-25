82bd92ab6f2a19629577ffcb8e1773502797fc94
maltsev <arelav90@mail.ru>
2018-05-25 10:28:55 +0300

    visual fixes

--- bars.py
+++ bars.py
@@ -3,67 +3,70 @@ import json
 
 def is_number(input_data):
     try:
-        float(input_data)
-        return float(input_data)
+        float_input_data = float(input_data)
+        return float_input_data
     except ValueError:
         return False
 
 
-def is_json(input_data):
+def open_json(path_to_file):
     try:
-        json.load(open(input_data, "r", encoding="utf8"))
-        return True
+        with open(path_to_file, "r", encoding="utf8") as opened_file:
+            json_data = json.load(opened_file)
+        return json_data["features"]
     except ValueError:
         return False
 
 
-def load_data(file_path):
-    with open(file_path, "r", encoding="utf8") as json_file:
-        all_bars_data = json.load(json_file)
-    return all_bars_data["features"]
-
-
-def get_biggest_bar(input_data):
-    biggest_bar_data = max(input_data,
-                           key=lambda x:
-                           x["properties"]["Attributes"]["SeatsCount"])
+def get_biggest_bar(bars):
+    biggest_bar_data = max(
+        bars,
+        key=lambda x: x["properties"]["Attributes"]["SeatsCount"])
     return biggest_bar_data
 
 
-def get_smallest_bar(input_data):
-    smallest_bar_data = min(input_data,
-                            key=lambda x:
-                            x["properties"]["Attributes"]["SeatsCount"])
+def get_smallest_bar(bars):
+    smallest_bar_data = min(
+        bars,
+        key=lambda x: x["properties"]["Attributes"]["SeatsCount"])
     return smallest_bar_data
 
 
-def get_closest_bar(input_data, longitude, latitude):
-    closest_bar_data = min(input_data,
-                           key=lambda x:
-                           ((float(x["geometry"]["coordinates"][0]) -
-                             (longitude)) ** 2 +
-                            (float(x["geometry"]["coordinates"][1]) -
-                             (latitude)) ** 2) ** (1 / 2))
+def get_closest_bar(bars, longitude, latitude):
+    closest_bar_data = min(
+        bars,
+        key=lambda x: ((float(x["geometry"]["coordinates"][0]) -
+                        (longitude))**2 +
+                       (float(x["geometry"]["coordinates"][1]) -
+                        (latitude))**2)**(1/2))
     return closest_bar_data
 
 
+'''
+Комментарий про сделать if короче не понял.
+Как ни крути, содержимое if не изменится в объемах.
+Можно немного подробнее описать рекомендации.
+'''
+
 if __name__ == "__main__":
-    path_to_file = "bars.json"
-    if is_json(path_to_file):
-        print("The biggest bar is ",
-              get_biggest_bar(load_data(path_to_file))
-              ["properties"]["Attributes"]["Name"])
+    input_path = input("Enter path: ")
+    if open_json(input_path):
+        print(
+            "The biggest bar is ",
+            get_biggest_bar(open_json(input_path))
+            ["properties"]["Attributes"]["Name"])
         print("The smallest bar is ",
-              get_smallest_bar(load_data(path_to_file))
+              get_smallest_bar(open_json(input_path))
               ["properties"]["Attributes"]["Name"])
         current_x_coord = is_number(input("Insert longitude: "))
         current_y_coord = is_number(input("Insert latitude: "))
         if is_number(current_x_coord) and is_number(current_y_coord):
             print("The closest bar is ",
-                  get_closest_bar(load_data(path_to_file),
-                                  current_x_coord, current_y_coord)
+                  get_closest_bar(open_json(input_path),
+                                  current_x_coord,
+                                  current_y_coord)
                   ["properties"]["Attributes"]["Name"])
         else:
             exit("you must enter numbers!")
     else:
-        print("Your input file is incorrect")
+        print("Your input file or path is incorrect")
